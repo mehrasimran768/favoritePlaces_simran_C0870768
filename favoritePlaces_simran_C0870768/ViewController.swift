e6f9ffb7c569b923328eb13c993d210eb6c894d7
simran mehra <simranmehra.iv@gmail.com>
2023-01-24 18:07:47 -0500

    Initial Commit

---
+++ favoritePlaces_simran_C0870768/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  favoritePlaces_simran_C0870768
+//
+//  Created by simran mehra on 2023-01-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
